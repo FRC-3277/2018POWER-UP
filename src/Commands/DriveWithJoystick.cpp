04b07ff383fc42da3798da5546d9c8f283374090
FRC-3277 <prodigi3277@gmail.com>
2018-01-31 17:59:10 -0600

    Mapping of Joystick Contoller to mapped buttons

--- src/Commands/DriveWithJoystick.cpp
+++ src/Commands/DriveWithJoystick.cpp
@@ -13,8 +13,12 @@ void DriveWithJoystick::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void DriveWithJoystick::Execute()
 {
-	Robot::driveTrain.get()->SetDrive(Robot::oi->getXBoxController()->GetRawAxis(XBOX_LEFT_STICK_X_AXIS),
-	Robot::oi.get()->getXBoxController()->GetRawAxis(XBOX_LEFT_STICK_Y_AXIS), Robot::oi.get()->getXBoxController()->GetRawAxis(XBOX_RIGHT_STICK_X_AXIS));
+	//TODO: feature switch xbox controller or joystick.
+	//Robot::driveTrain.get()->SetDrive(Robot::oi->getXBoxController()->GetRawAxis(XBOX_LEFT_STICK_X_AXIS),
+	//Robot::oi.get()->getXBoxController()->GetRawAxis(XBOX_LEFT_STICK_Y_AXIS), Robot::oi.get()->getXBoxController()->GetRawAxis(XBOX_RIGHT_STICK_X_AXIS));
+	Robot::driveTrain.get()->SetDrive(Robot::oi->getXBoxController()->GetRawAxis(JOYSTICK_X_AXIS),
+	Robot::oi.get()->getXBoxController()->GetRawAxis(JOYSTICK_Y_AXIS), Robot::oi.get()->getXBoxController()->GetRawAxis(JOYSTICK_Z_AXIS));
+
 }
 
 // Make this return true when this Command no longer needs to run execute()
