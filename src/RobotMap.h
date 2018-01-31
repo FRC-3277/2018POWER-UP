0f6ad77c77680e2ede042ef6623f5e70bd3fffe3
FRC-3277 <prodigi3277@gmail.com>
2018-01-31 17:58:10 -0600

    Changed the IDs for sanity

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -15,7 +15,7 @@
  */
 
 // DriveTrain
-static constexpr int FRONT_LEFT_MOTOR_CAN_ID = 2;
-static constexpr int FRONT_RIGHT_MOTOR_CAN_ID = 4;
+static constexpr int FRONT_LEFT_MOTOR_CAN_ID = 1;
+static constexpr int FRONT_RIGHT_MOTOR_CAN_ID = 2;
 static constexpr int REAR_LEFT_MOTOR_CAN_ID = 3;
-static constexpr int REAR_RIGHT_MOTOR_CAN_ID = 5;
+static constexpr int REAR_RIGHT_MOTOR_CAN_ID = 4;
