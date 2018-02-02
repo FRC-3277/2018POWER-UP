d2feca73abd60e869175ba2e0b443726f5081a06
FRC-3277 <prodigi3277@gmail.com>
2018-02-01 19:49:17 -0600

    Adding Elevator subsystem (In progress).

---
+++ src/Subsystems/Elevator.cpp
@@ -0,0 +1,14 @@
+#include "Elevator.h"
+#include "../RobotMap.h"
+
+Elevator::Elevator() : Subsystem("ExampleSubsystem") {
+
+}
+
+void Elevator::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ src/Subsystems/Elevator.h
@@ -0,0 +1,16 @@
+#ifndef Elevator_H
+#define Elevator_H
+
+#include <Commands/Subsystem.h>
+
+class Elevator : public Subsystem {
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+
+public:
+	Elevator();
+	void InitDefaultCommand();
+};
+
+#endif  // Elevator_H
