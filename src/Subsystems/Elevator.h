1ede8ccffa0ac5286a1bcc2b9e02ecf3884ad3fe
Ethan Toso <18ethan.toso@myprowler.org>
2018-02-02 19:33:16 -0600

    Adding Elevator Subsystem

--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -3,7 +3,7 @@
 
 #include <Commands/Subsystem.h>
 
-class Elevator : public Subsystem {
+class Elevator : public frc::Subsystem {
 private:
 	// It's desirable that everything possible under private except
 	// for methods that implement subsystem capabilities
