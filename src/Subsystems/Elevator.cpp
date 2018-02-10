d9000c42906c42a862b16981c3fc4c7dc13a151d
FRC-3277 <prodigi3277@gmail.com>
2018-02-10 17:14:40 -0600

    More disabling on elevator subsystem to allow for testing.  Limit switches don't exist causing ghosting.

--- src/Subsystems/Elevator.cpp
+++ src/Subsystems/Elevator.cpp
@@ -91,7 +91,8 @@ void Elevator::RaiseElevator()
 
 	if(MaxHeightLimitSwitch->Get())
 	{
-		speed = StopElevatorSpeed;
+		//TODO: Re-enable once this is actually installed
+		//speed = StopElevatorSpeed;
 	}
 
 	LeftElevatorTalon->Set(speed);
@@ -107,7 +108,8 @@ void Elevator::LowerElevator()
 
 	if(MinHeightLimitSwitch->Get())
 	{
-		speed = StopElevatorSpeed;
+		//TODO: Re-enable once this is actually installed
+		//speed = StopElevatorSpeed;
 	}
 
 	LeftElevatorTalon->Set(speed);
