cd538181bb487f7e19acb4e7f2fde506a0b20fdb
ProDigi3277 <prodigi3277@gmail.com>
2018-02-15 02:17:43 -0600

    Added log every n for every remaining logging level for uniform sanity.

--- src/LumberJack.h
+++ src/LumberJack.h
@@ -52,18 +52,40 @@ public:
 	void SetConfig();
 	void iLog(char* msg);
 	void iLog(char* msg, int loggingFrequency);
+
 	void dLog(char* msg);
+	void dLog(char* msg, int loggingFrequency);
+
 	void eLog(char* msg);
+	void eLog(char* msg, int loggingFrequency);
+
 	void wLog(char* msg);
+	void wLog(char* msg, int loggingFrequency);
+
 	void fLog(char* msg);
+	void fLog(char* msg, int loggingFrequency);
+
 	void tLog(char* msg);
+	void tLog(char* msg, int loggingFrequency);
+
 	void iLog(const string& msg);
 	void iLog(const string& msg, int loggingFrequency);
+
 	void dLog(const string& msg);
+	void dLog(const string& msg, int loggingFrequency);
+
 	void eLog(const string& msg);
+	void eLog(const string& msg, int loggingFrequency);
+
 	void wLog(const string& msg);
+	void wLog(const string& msg, int loggingFrequency);
+
 	void fLog(const string& msg);
+	void fLog(const string& msg, int loggingFrequency);
+
 	void tLog(const string& msg);
+	void tLog(const string& msg, int loggingFrequency);
+
 	void dashLogData(llvm::StringRef key, Sendable* data);
 	void dashLogNumber(llvm::StringRef key, double value);
 	void dashLogString(llvm::StringRef key, llvm::StringRef message);
