00f3c30cb67bb3bb0c90f3bc6669d739d799e571
Ethan Toso <18ethan.toso@myprowler.org>
2018-02-14 20:28:20 -0600

    Adding Autonomous Commands

---
+++ src/Commands/AutonomousDriveCommand.h
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <Commands/Command.h>
+#include "Robot.h"
+#include "LumberJack.h"
+#include <chrono>
+#include <ctime>
+
+class AutonomousDriveCommand : public frc::Command {
+
+private:
+	double lateral;
+	double forwardBackward;
+	double rotation;
+	double AutonomousDriveWaitPeriod;
+
+	bool EndAutonomousDriveCommand = false;
+
+	std::chrono::system_clock::time_point AutonomousDriveBegin;
+	std::chrono::system_clock::time_point AutonomousDriveCurrent;
+
+
+public:
+	AutonomousDriveCommand(double lateral, double forwardBackward, double rotation, double AutonomousDriveWaitPeriod);
+	void Initialize() override;
+	void Execute() override;
+	bool IsFinished() override;
+	void End() override;
+	void Interrupted() override;
+};
+
