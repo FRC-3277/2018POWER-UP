71781b2e351be120851fa2e51e7a6028a7a5ae55
ProDigi3277 <prodigi3277@gmail.com>
2018-02-16 15:24:44 -0600

    Remove unused commented out smoothing averaging code.

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -119,48 +119,6 @@ void DriveTrain::SetDrive(double lateral, double forwardBackward, double rotatio
 	driveTrainDebugInfo += std::string("Rotation: ") + std::to_string(rotation);
 	lumberJack->dLog(driveTrainDebugInfo);
 
-	// Smooth out speed if going fast
-	// Approx Average
-	/*
-	AverageLateral = ApproxRollingAverage(AverageLateral, lateral);
-	AverageForwardBackward = ApproxRollingAverage(AverageForwardBackward, forwardBackward);
-	AverageRotation = ApproxRollingAverage(AverageRotation, rotation);
-
-	if(lateral >= ValueToTriggerAverageOverride ||
-		forwardBackward >= ValueToTriggerAverageOverride ||
-		rotation >= ValueToTriggerAverageOverride)
-	{
-		lateral = AverageLateral;
-		forwardBackward = AverageForwardBackward;
-		rotation = AverageRotation;
-	}
-
-
-	// Actual Average
-	AverageLateralArray[AverageArrayIterator] = lateral;
-	AverageForwardBackwardArray[AverageArrayIterator] = forwardBackward;
-	AverageRotationArray[AverageArrayIterator] = rotation;
-	if(fabs(lateral) >= ValueToTriggerAverageOverride ||
-		fabs(forwardBackward) >= ValueToTriggerAverageOverride ||
-		fabs(rotation) >= ValueToTriggerAverageOverride)
-	{
-		lumberJack->iLog(std::string("lateral: ") + std::to_string(lateral) +
-				std::string("forwardBackward: ") + std::to_string(forwardBackward) +
-				std::string("rotation: ") + std::to_string(rotation) +
-				std::string("ValueToTriggerAverageOverride: ") + std::to_string(ValueToTriggerAverageOverride));
-		lateral = ActualAverage(AverageLateralArray, NumberOfDataPointsForAverage);
-		forwardBackward = ActualAverage(AverageForwardBackwardArray, NumberOfDataPointsForAverage);
-		rotation = ActualAverage(AverageRotationArray, NumberOfDataPointsForAverage);
-	}
-
-	// Prevent out of bounds
-	if(AverageArrayIterator++ > 100)
-	{
-		AverageArrayIterator = 0;
-	}
-	*/
-
-
 	// Last modification before interacting with the drivetrain
 	if(InvertDriverControls)
 	{
@@ -199,6 +157,7 @@ double DriveTrain::ActualAverage(double *Array, int ArraySize)
     {
         sum += Array[i];
     }
+
     return sum/denominator;
 }
 
