081af688e47e53c97de8df86bd683d1271b2d19a
Ethan Toso <18ethan.toso@myprowler.org>
2018-02-15 20:13:30 -0600

    g-code Language definitions

--- src/Commands/AutonomousScenarios.cpp
+++ src/Commands/AutonomousScenarios.cpp
@@ -12,6 +12,18 @@ void AutonomousScenarios::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void AutonomousScenarios::Execute() {
 
+/** g-code language (Subject to change)
+ * D(forward/backwards) = Drive forwards/backwards
+ * T(rotation) = Turn; Positive for Right and Negative for Left
+ * L(lateral) = Lateral; Positive for Right and Negative for Left
+ * S = Stop Everything
+ * ER = Raise Elevator
+ * EL = Lower Elevator
+ * E(setpoint) = Go to Elevator Preset
+ * GE = Eat Cube
+ * GS = Spit Cube
+ */
+
 }
 
 // Make this return true when this Command no longer needs to run execute()
