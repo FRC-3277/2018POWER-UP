f0ed619bdfb14983fdba7873a9d013a896cd0906
ProDigi3277 <prodigi3277@gmail.com>
2018-02-25 02:18:12 -0600

    2 additional controllers now available.

--- src/Controller/Extreme3DPro.cpp
+++ src/Controller/Extreme3DPro.cpp
@@ -9,51 +9,142 @@ namespace UserDefinedController {
 
 	Extreme3D_Pro::~Extreme3D_Pro()
 	{
-
 	}
 
 	void Extreme3D_Pro::AssignButton(const std::string& givenButtonName,
-									std::shared_ptr<frc::JoystickButton> joystickButton,
-									Extreme3D_Pro::LOGITECH_EXTREME3DPRO_BUTTON buttonToButtonNumber)
+									std::shared_ptr<frc::Joystick> controller,
+									std::shared_ptr<frc::JoystickButton> controllerButton,
+									LOGITECH_EXTREME3DPRO_BUTTON buttonToButtonNumber)
 	{
-		ButtonToButtonNumber givenButtonMapping;
-		givenButtonMapping.joystickButton = joystickButton;
-		givenButtonMapping.LOGITECH_EXTREME3DPRO_BUTTON = buttonToButtonNumber;
+		ControllerButtonMapping buttonMapping;
+		buttonMapping.controller = controller;
+		buttonMapping.controllerButton = controllerButton;
+		buttonMapping.buttonNumber = buttonToButtonNumber;
 
 		// TODO: prevent duplicate assignment to same button and/or same button mapping
-		userDefinedControllerButtonMapping.insert(std::make_pair(givenButtonName, givenButtonMapping));
+		userDefinedControllerButtonMapping.insert(std::make_pair(givenButtonName, buttonMapping));
 	}
 
-	void Extreme3D_Pro::AssignAxis(const std::string& givenAxisName, Extreme3D_Pro::LOGITECH_EXTREME3DPRO_AXIS givenAxisNumber)
+	void Extreme3D_Pro::AssignAxis(const std::string& givenAxisName,
+									std::shared_ptr<frc::Joystick> controller,
+									Extreme3D_Pro::LOGITECH_EXTREME3DPRO_AXIS axisToAxisNumber)
 	{
+		ControllerAxisMapping axisMapping;
+		axisMapping.controller = controller;
+		axisMapping.axisNumber = axisToAxisNumber;
 		// TODO: prevent duplicate assignment to same axis mapping
-		userDefinedControllerAxisMapping.insert(std::make_pair(givenAxisName, givenAxisNumber));
+		userDefinedControllerAxisMapping.insert(std::make_pair(givenAxisName, axisMapping));
 	}
 
-	Extreme3D_Pro::ButtonToButtonNumber Extreme3D_Pro::GetButton(const std::string& givenButtonName)
+	Extreme3D_Pro::ControllerButtonMapping Extreme3D_Pro::GetButtonMap(const std::string& givenButtonName)
 	{
-		std::map<std::string, ButtonToButtonNumber>::iterator iteratorPointer;
-		Extreme3D_Pro::ButtonToButtonNumber controllerButtonMapping = { 0 };
+		std::map<std::string, ControllerButtonMapping>::iterator iteratorPointer;
+		ControllerButtonMapping controllerButtonMapping;
 
 		iteratorPointer = userDefinedControllerButtonMapping.find(givenButtonName);
-		  if(iteratorPointer != userDefinedControllerButtonMapping.end())
-			  controllerButtonMapping = iteratorPointer->second;
-		  else
-		    lumberJack->eLog("Requested button not found: " + givenButtonName);
+		if(iteratorPointer != userDefinedControllerButtonMapping.end())
+		{
+		  controllerButtonMapping = iteratorPointer->second;
+		}
+		else
+		{
+			lumberJack->eLog("Requested button map not found: " + givenButtonName);
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested button map not found; " + givenButtonName);
+		}
 
-		  return controllerButtonMapping;
+		return controllerButtonMapping;
 	}
 
-	int Extreme3D_Pro::GetAxis(const std::string& givenAxisName)
+	Extreme3D_Pro::ControllerAxisMapping Extreme3D_Pro::GetAxisMap(const std::string& givenAxisName)
 	{
-		std::map<std::string, LOGITECH_EXTREME3DPRO_AXIS>::iterator iteratorPointer;
-		LOGITECH_EXTREME3DPRO_AXIS axisNumber = LOGITECH_EXTREME3DPRO_AXIS::INVALID_AXIS_NUMBER;
+		std::map<std::string, ControllerAxisMapping>::iterator iteratorPointer;
+		ControllerAxisMapping controllerAxisMapping;
 
 		iteratorPointer = userDefinedControllerAxisMapping.find(givenAxisName);
 		if(iteratorPointer != userDefinedControllerAxisMapping.end())
-			axisNumber = iteratorPointer->second;
+		{
+			controllerAxisMapping = iteratorPointer->second;
+		}
 		else
+		{
+			lumberJack->eLog("Requested axis map not found: " + givenAxisName);
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested axis map not found; " + givenAxisName);
+		}
+
+		return controllerAxisMapping;
+	}
+
+	std::shared_ptr<frc::Joystick> Extreme3D_Pro::GetController(const std::string& givenButtonOrAxisName)
+	{
+		std::shared_ptr<frc::Joystick> controller;
+
+		try
+		{
+			controller = GetButtonMap(givenButtonOrAxisName).controller;
+		}
+		catch(ERR::CustomError& e)
+		{
+			// If not mapped to button then check for axis
+			try
+			{
+				controller = GetAxisMap(givenButtonOrAxisName).controller;
+			}
+			catch(ERR::CustomError& e)
+			{
+				throw ERR::CustomError(std::string(__FILE__) + "; Requested controller not found; " + givenButtonOrAxisName);
+			}
+		}
+
+		return controller;
+	}
+
+	std::shared_ptr<frc::JoystickButton> Extreme3D_Pro::GetButton(const std::string& givenButtonName)
+	{
+		std::shared_ptr<frc::JoystickButton> controllerButton;
+
+		try
+		{
+			controllerButton = GetButtonMap(givenButtonName).controllerButton;
+		}
+		catch(ERR::CustomError& e)
+		{
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested button not found; " + givenButtonName);
+		}
+
+		return controllerButton;
+	}
+
+	int Extreme3D_Pro::GetButtonNumber(const std::string& givenButtonName)
+	{
+		LOGITECH_EXTREME3DPRO_BUTTON buttonNumber;
+
+		try
+		{
+			buttonNumber = GetButtonMap(givenButtonName).buttonNumber;
+		}
+		catch(ERR::CustomError& e)
+		{
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested button number not found; " + givenButtonName);
+		}
+
+		return static_cast<int>(buttonNumber);
+	}
+
+	int Extreme3D_Pro::GetAxisNumber(const std::string& givenAxisName)
+	{
+		std::map<std::string, ControllerAxisMapping>::iterator iteratorPointer;
+		LOGITECH_EXTREME3DPRO_AXIS axisNumber;
+
+		iteratorPointer = userDefinedControllerAxisMapping.find(givenAxisName);
+		if(iteratorPointer != userDefinedControllerAxisMapping.end())
+		{
+			axisNumber = iteratorPointer->second.axisNumber;
+		}
+		else
+		{
 			lumberJack->eLog("Requested axis not found: " + givenAxisName);
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested axis not found; " + givenAxisName);
+		}
 
 		return static_cast<int>(axisNumber);
 	}
@@ -62,32 +153,32 @@ namespace UserDefinedController {
 	{
 		// First the buttons
 		// Create a map iterator and point to beginning of map
-		std::map<std::string, ButtonToButtonNumber>::iterator buttonBeginIterator = userDefinedControllerButtonMapping.begin();
-		std::map<std::string, ButtonToButtonNumber>::iterator buttonEndIterator = userDefinedControllerButtonMapping.end();
+		std::map<std::string, ControllerButtonMapping>::iterator buttonBeginIterator = userDefinedControllerButtonMapping.begin();
+		std::map<std::string, ControllerButtonMapping>::iterator buttonEndIterator = userDefinedControllerButtonMapping.end();
 
 		// Iterate over a map using std::for_each and Lambda function
-		std::for_each(buttonBeginIterator, buttonEndIterator, [](std::pair<std::string, Extreme3D_Pro::LOGITECH_EXTREME3DPRO_BUTTON> element)
+		std::for_each(buttonBeginIterator, buttonEndIterator, [](std::pair<std::string, ControllerButtonMapping> element)
 		{
 			// Accessing KEY from element
 			std::string givenButtonName = element.first;
 			// Accessing VALUE from element.
-			LOGITECH_EXTREME3DPRO_BUTTON buttonNumber= element.second;
-			lumberJack->iLog("Extreme3D_Pro button name: " + givenButtonName + "; Button number: " + std::to_string(static_cast<int>(buttonNumber)));
+			ControllerButtonMapping buttonMapping= element.second;
+			//lumberJack->iLog("Extreme3D_Pro button name: " + givenButtonName + "; Button number: " + std::to_string(static_cast<int>(buttonMapping.buttonNumber)));
 		});
 
 		// Last the axis
 		// Create a map iterator and point to beginning of map
-		std::map<std::string, int>::iterator axisBeginIterator = userDefinedControllerAxisMapping.begin();
-		std::map<std::string, int>::iterator axisEndIterator = userDefinedControllerAxisMapping.begin();
+		std::map<std::string, ControllerAxisMapping>::iterator axisBeginIterator = userDefinedControllerAxisMapping.begin();
+		std::map<std::string, ControllerAxisMapping>::iterator axisEndIterator = userDefinedControllerAxisMapping.begin();
 
 		// Iterate over a map using std::for_each and Lambda function
-		std::for_each(axisBeginIterator, axisEndIterator, [](std::pair<std::string, Extreme3D_Pro::LOGITECH_EXTREME3DPRO_AXIS> element)
+		std::for_each(axisBeginIterator, axisEndIterator, [](std::pair<std::string, ControllerAxisMapping> element)
 		{
 			// Accessing KEY from element
 			std::string givenAxisName = element.first;
 			// Accessing VALUE from element.
-			LOGITECH_EXTREME3DPRO_AXIS axisNumber = element.second;
-			lumberJack->iLog("Extreme3D_Pro axis name: " + givenAxisName + "; Axis number: " + std::to_string(static_cast<int>(axisNumber)));
+			LOGITECH_EXTREME3DPRO_AXIS axisNumber = element.second.axisNumber;
+			//lumberJack->iLog("Extreme3D_Pro axis name: " + givenAxisName + "; Axis number: " + std::to_string(static_cast<int>(axisNumber)));
 		});
 	}
 
--- src/Controller/XBox.cpp
+++ src/Controller/XBox.cpp
@@ -1,14 +1,184 @@
 #include "Controller/XBox.h"
 
-namespace UserDefinedController {
+namespace UserDefinedController
+{
+	XBox::XBox()
+	{
+		lumberJack.reset(new LumberJack());
+	}
 
-XBox::XBox() {
-	// TODO Auto-generated constructor stub
+	XBox::~XBox()
+	{
+	}
 
-}
+	void XBox::AssignButton(const std::string& givenButtonName,
+							std::shared_ptr<frc::Joystick> controller,
+							std::shared_ptr<frc::JoystickButton> controllerButton,
+							XBOX_BUTTON buttonToButtonNumber)
+	{
+		ControllerButtonMapping buttonMapping;
+		buttonMapping.controller = controller;
+		buttonMapping.controllerButton = controllerButton;
+		buttonMapping.buttonNumber = buttonToButtonNumber;
 
-XBox::~XBox() {
-	// TODO Auto-generated destructor stub
-}
+		// TODO: prevent duplicate assignment to same button and/or same button mapping
+		userDefinedControllerButtonMapping.insert(std::make_pair(givenButtonName, buttonMapping));
+	}
 
+	void XBox::AssignAxis(const std::string& givenAxisName,
+							std::shared_ptr<frc::Joystick> controller,
+							XBox::XBOX_AXIS axisToAxisNumber)
+	{
+		ControllerAxisMapping axisMapping;
+		axisMapping.controller = controller;
+		axisMapping.axisNumber = axisToAxisNumber;
+		// TODO: prevent duplicate assignment to same axis mapping
+		userDefinedControllerAxisMapping.insert(std::make_pair(givenAxisName, axisMapping));
+	}
+
+	XBox::ControllerButtonMapping XBox::GetButtonMap(const std::string& givenButtonName)
+	{
+		std::map<std::string, ControllerButtonMapping>::iterator iteratorPointer;
+		ControllerButtonMapping controllerButtonMapping;
+
+		iteratorPointer = userDefinedControllerButtonMapping.find(givenButtonName);
+		if(iteratorPointer != userDefinedControllerButtonMapping.end())
+		{
+		  controllerButtonMapping = iteratorPointer->second;
+		}
+		else
+		{
+			lumberJack->eLog("Requested button map not found: " + givenButtonName);
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested button map not found; " + givenButtonName);
+		}
+
+		return controllerButtonMapping;
+	}
+
+	XBox::ControllerAxisMapping XBox::GetAxisMap(const std::string& givenAxisName)
+	{
+		std::map<std::string, ControllerAxisMapping>::iterator iteratorPointer;
+		ControllerAxisMapping controllerAxisMapping;
+
+		iteratorPointer = userDefinedControllerAxisMapping.find(givenAxisName);
+		if(iteratorPointer != userDefinedControllerAxisMapping.end())
+		{
+			controllerAxisMapping = iteratorPointer->second;
+		}
+		else
+		{
+			lumberJack->eLog("Requested axis map not found: " + givenAxisName);
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested axis map not found; " + givenAxisName);
+		}
+
+		return controllerAxisMapping;
+	}
+
+	std::shared_ptr<frc::Joystick> XBox::GetController(const std::string& givenButtonOrAxisName)
+	{
+		std::shared_ptr<frc::Joystick> controller;
+
+		try
+		{
+			controller = GetButtonMap(givenButtonOrAxisName).controller;
+		}
+		catch(ERR::CustomError& e)
+		{
+			// If not mapped to button then check for axis
+			try
+			{
+				controller = GetAxisMap(givenButtonOrAxisName).controller;
+			}
+			catch(ERR::CustomError& e)
+			{
+				throw ERR::CustomError(std::string(__FILE__) + "; Requested controller not found; " + givenButtonOrAxisName);
+			}
+		}
+
+		return controller;
+	}
+
+	std::shared_ptr<frc::JoystickButton> XBox::GetButton(const std::string& givenButtonName)
+	{
+		std::shared_ptr<frc::JoystickButton> controllerButton;
+
+		try
+		{
+			controllerButton = GetButtonMap(givenButtonName).controllerButton;
+		}
+		catch(ERR::CustomError& e)
+		{
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested button not found; " + givenButtonName);
+		}
+
+		return controllerButton;
+	}
+
+	int XBox::GetButtonNumber(const std::string& givenButtonName)
+	{
+		XBOX_BUTTON buttonNumber;
+
+		try
+		{
+			buttonNumber = GetButtonMap(givenButtonName).buttonNumber;
+		}
+		catch(ERR::CustomError& e)
+		{
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested button number not found; " + givenButtonName);
+		}
+
+		return static_cast<int>(buttonNumber);
+	}
+
+	int XBox::GetAxisNumber(const std::string& givenAxisName)
+	{
+		std::map<std::string, ControllerAxisMapping>::iterator iteratorPointer;
+		XBOX_AXIS axisNumber;
+
+		iteratorPointer = userDefinedControllerAxisMapping.find(givenAxisName);
+		if(iteratorPointer != userDefinedControllerAxisMapping.end())
+		{
+			axisNumber = iteratorPointer->second.axisNumber;
+		}
+		else
+		{
+			lumberJack->eLog("Requested axis not found: " + givenAxisName);
+			throw ERR::CustomError(std::string(__FILE__) + "; Requested axis not found; " + givenAxisName);
+		}
+
+		return static_cast<int>(axisNumber);
+	}
+
+	void XBox::DebugReportControllerUsage()
+	{
+		// First the buttons
+		// Create a map iterator and point to beginning of map
+		std::map<std::string, ControllerButtonMapping>::iterator buttonBeginIterator = userDefinedControllerButtonMapping.begin();
+		std::map<std::string, ControllerButtonMapping>::iterator buttonEndIterator = userDefinedControllerButtonMapping.end();
+
+		// Iterate over a map using std::for_each and Lambda function
+		std::for_each(buttonBeginIterator, buttonEndIterator, [](std::pair<std::string, ControllerButtonMapping> element)
+		{
+			// Accessing KEY from element
+			std::string givenButtonName = element.first;
+			// Accessing VALUE from element.
+			ControllerButtonMapping buttonMapping= element.second;
+			//lumberJack->iLog("Extreme3D_Pro button name: " + givenButtonName + "; Button number: " + std::to_string(static_cast<int>(buttonMapping.buttonNumber)));
+		});
+
+		// Last the axis
+		// Create a map iterator and point to beginning of map
+		std::map<std::string, ControllerAxisMapping>::iterator axisBeginIterator = userDefinedControllerAxisMapping.begin();
+		std::map<std::string, ControllerAxisMapping>::iterator axisEndIterator = userDefinedControllerAxisMapping.begin();
+
+		// Iterate over a map using std::for_each and Lambda function
+		std::for_each(axisBeginIterator, axisEndIterator, [](std::pair<std::string, ControllerAxisMapping> element)
+		{
+			// Accessing KEY from element
+			std::string givenAxisName = element.first;
+			// Accessing VALUE from element.
+			XBOX_AXIS axisNumber = element.second.axisNumber;
+			//lumberJack->iLog("Extreme3D_Pro axis name: " + givenAxisName + "; Axis number: " + std::to_string(static_cast<int>(axisNumber)));
+		});
+	}
 } /* namespace Controller */
