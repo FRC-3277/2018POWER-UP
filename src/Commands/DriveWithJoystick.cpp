c9a438c940accfb29b8e3f77126c607393f892e7
ProDigi3277 <prodigi3277@gmail.com>
2018-02-25 02:19:13 -0600

    Remove commented out stuff.

--- src/Commands/DriveWithJoystick.cpp
+++ src/Commands/DriveWithJoystick.cpp
@@ -12,9 +12,6 @@ void DriveWithJoystick::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void DriveWithJoystick::Execute()
 {
-	//TODO: feature switch xbox controller or joystick.
-	//Robot::driveTrain.get()->SetDrive(Robot::oi->getXBoxController()->GetRawAxis(XBOX_LEFT_STICK_X_AXIS),
-	//Robot::oi.get()->getXBoxController()->GetRawAxis(XBOX_LEFT_STICK_Y_AXIS), Robot::oi.get()->getXBoxController()->GetRawAxis(XBOX_RIGHT_STICK_X_AXIS));
 	Robot::driveTrain.get()->SetDrive(Robot::oi->GetJoystickX(), Robot::oi.get()->GetJoystickY(), Robot::oi.get()->GetJoystickTwist());
 
 }
