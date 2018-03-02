56d1858d590a363db514e3e2958a4b987266b9e6
FRC-3277 <prodigi3277@gmail.com>
2018-03-01 18:26:03 -0600

    Making winch faster

--- src/Subsystems/Lifter.h
+++ src/Subsystems/Lifter.h
@@ -27,7 +27,7 @@ private:
 	static constexpr int LeftEjectCoreServoAngleDefault = 180;
 	static constexpr int RightEjectCoreServoAngleDefault = 0;
 
-	static constexpr double WinchDefaultSpeed = -0.25;
+	static constexpr double WinchDefaultSpeed = -0.99;
 
 	bool IsCorePreparedToBeEjected = false;
 	bool EnableLifterSubsystemLast30Seconds = false;
