d3adaf67db57e381cd070a3949f8ca1acdba3b80
FRC-3277 <prodigi3277@gmail.com>
2018-03-03 10:43:16 -0600

    Correcting limit switch numbers.

--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -79,12 +79,12 @@ private:
 public:
 	Elevator();
 
-	static constexpr int MAX_LIMIT_SWITCH_NUMBER = 6;
-	static constexpr int HIGH_LIMIT_SWITCH_NUMBER = 5;
-	static constexpr int EJECT_CORE_LIMIT_SWITCH_NUMBER = 4;
-	static constexpr int MED_LIMIT_SWITCH_NUMBER = 3;
-	static constexpr int LOW_LIMIT_SWITCH_NUMBER = 2;
-	static constexpr int MIN_LIMIT_SWITCH_NUMBER = 1;
+	static constexpr int MAX_LIMIT_SWITCH_NUMBER = 5;
+	static constexpr int HIGH_LIMIT_SWITCH_NUMBER = 4;
+	static constexpr int EJECT_CORE_LIMIT_SWITCH_NUMBER = 3;
+	static constexpr int MED_LIMIT_SWITCH_NUMBER = 2;
+	static constexpr int LOW_LIMIT_SWITCH_NUMBER = 1;
+	static constexpr int MIN_LIMIT_SWITCH_NUMBER = 0;
 
 	void InitDefaultCommand();
 	void RaiseElevator();
