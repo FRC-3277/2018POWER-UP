bc1297f392f18e1ec2b624c709e4cb8e6e6ae489
FRC-3277 <prodigi3277@gmail.com>
2018-03-29 10:07:05 -0500

    Removing unnecessary things to make the robot lighter.

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -36,7 +36,6 @@ static constexpr int ELEVATOR_EJECT_CORE_LIMIT_SWITCH_ID = 6;
 static constexpr int GRABBER_LEFT_MOTOR_CAN_ID = 20;
 static constexpr int GRABBER_RIGHT_MOTOR_CAN_ID = 21;
 static constexpr int BLOCK_INJECTION_STOP_LIMIT_SWITCH_ID = 0;
-static constexpr int BLOCK_EJECTION_STOP_LIMIT_SWITCH_ID = 1;
 static constexpr int BLOCK_TILT_UP_STOP_LIMIT_SWITCH_ID = 2;
 static constexpr int BLOCK_TILT_DOWN_STOP_LIMIT_SWITCH_ID = 3;
 
