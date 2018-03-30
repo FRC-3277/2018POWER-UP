7a2b2d0c98c8e97617a0130ec22fbd10e5819222
FRC-3277 <prodigi3277@gmail.com>
2018-03-30 09:55:40 -0500

    Fix spit cube auton.

--- src/Commands/AutonomousSpitCubeCommand.cpp
+++ src/Commands/AutonomousSpitCubeCommand.cpp
@@ -15,14 +15,15 @@ AutonomousSpitCubeCommand::AutonomousSpitCubeCommand()
 // Called just before this Command runs the first time
 void AutonomousSpitCubeCommand::Initialize()
 {
+	Robot::grabber->SetAutonTimerStart();
 	Robot::grabber->SetGrabberSpitSpeed(0.99);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void AutonomousSpitCubeCommand::Execute()
 {
-	Robot::grabber->SetAutonTimerStart();
-	while(Robot::grabber->GetAutonTimerCurrent() <= AutonSpitTimeoutMilli)
+	while(Robot::grabber->GetAutonTimerCurrent() <= AutonSpitTimeoutMilli &&
+		Robot::grabber->GetLimitSwitch())
 	{
 		Robot::grabber->SpitCube();
 	}
@@ -31,20 +32,19 @@ void AutonomousSpitCubeCommand::Execute()
 // Make this return true when this Command no longer needs to run execute()
 bool AutonomousSpitCubeCommand::IsFinished()
 {
-	printf("IsFinished\n");
-	//return Robot::grabber->GetAutonTimerCurrent() > AutonSpitTimeoutMilli;
+	Robot::grabber->GrabberStop();
 	return true;
 }
 
 // Called once after isFinished returns true
 void AutonomousSpitCubeCommand::End()
 {
-	printf("End\n");
+	Robot::grabber->GrabberStop();
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void AutonomousSpitCubeCommand::Interrupted()
 {
-	printf("Interrupted\n");
+	Robot::grabber->GrabberStop();
 }
